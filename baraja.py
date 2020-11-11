2e81e89f7c701b78e00444b7182564258cd5ddc2
Ismael Saleh Sanchez <ismaelsalehsanchez@Ghisp.local>
2020-11-11 23:18:09 +0100

    crear baraja con barajeo

---
+++ baraja.py
@@ -0,0 +1,33 @@
+import random
+
+palos = ["o", "c", "e", "b"]
+numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
+
+# funcion para crear baraja
+def creaBaraja():
+    baraja = []
+    for palo in palos:
+        for numero in numeros:
+            baraja.append(numero + palo)
+    return baraja
+
+# funcion para intercambiar cartas
+def intercambio(primer_valor, segundo_valor):
+    aux = primer_valor
+    primer_valor = segundo_valor
+    segundo_valor = aux
+    return primer_valor, segundo_valor
+
+# funcion para crear barajar
+def barajar(lista_de_naipes):
+    for i in range (len(lista_de_naipes)):
+        nueva_pos = random.randrange(len(lista_de_naipes))
+
+        #intercambio de cartas con tecnica de vaso vacio
+        aux = lista_de_naipes[nueva_pos]
+        lista_de_naipes[nueva_pos] = lista_de_naipes[i]
+        lista_de_naipes[i] = aux
+    return lista_de_naipes
+
+# crear baraja, barajar y mostrar resultado por separado
+print (barajar(creaBaraja()))
