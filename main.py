500acdc776ef8e39176618af2a0d66c5b5e29be8
Ismael Saleh Sanchez <ismaelsalehsanchez@Ghisp.local>
2020-11-12 22:09:43 +0100

    Metodo repartir en clase baraja

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ numeros = ["A", "2", "3", "4", "5", "6", "7", "S", "C", "R"]
 
 miBaraja = barajac.Baraja(palos, numeros)
 
+miBaraja.mazacote[2]
+
 print(miBaraja.mazacote)
+
+print(miBaraja.repartir(3,5))
+
 miBaraja.barajar()
 print(miBaraja.mazacote)
